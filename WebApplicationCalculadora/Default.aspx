ca4f529d94f4e164516057ceb98f993cb2a2fe1f
jmcasal-ar <casal.juanmanuel@gmail.com>
2019-07-15 20:23:28 -0300

    Calculadora ASP.NET
    Calculadora elaborada en ASP.NET

---
+++ WebApplicationCalculadora/Default.aspx
@@ -0,0 +1,92 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplicationCalculadora.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table border="0">
+            <tr>
+                <td colspan="4">
+                    <asp:TextBox ID="TextBoxExpresionAritmetica" Text="" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="ButtonAbrirParentesis"  runat="server" Text="(" OnClick="ButtonAbrirParentesis_Click"/>
+                </td>
+                <td>
+                    <asp:Button ID="ButtonCerrarParentesis" runat="server" Text=")" OnClick="ButtonCerrarParentesis_Click" />
+                </td>
+                <td>
+                    <asp:Button ID="ButtonPorcentaje" runat="server" Text="%" OnClick="ButtonPorcentaje_Click" />
+                </td>
+                <td>
+                    <asp:Button ID="ButtonBorrar" runat="server" Text="Delete" OnClick="ButtonBorrar_Click" />
+                </td>
+            </tr>
+             <tr>
+                <td>
+                    <asp:Button ID="Button7" runat="server" Text="7" OnClick="Button7_Click" BackColor="#FFD9C6" />
+                </td>
+                <td>
+                    <asp:Button ID="Button8" runat="server" Text="8" OnClick="Button8_Click" BackColor="#FFD9C6" />
+                </td>
+                <td>
+                    <asp:Button ID="Button9" runat="server" Text="9" OnClick="Button9_Click" BackColor="#FFD9C6" />
+                </td>
+                <td>
+                    <asp:Button ID="ButtonDividir" runat="server" Text="/" OnClick="ButtonDividir_Click" />
+                </td>
+            </tr>
+             <tr>
+                <td>
+                    <asp:Button ID="Button4" runat="server" Text="4" OnClick="Button4_Click" BackColor="#FFD9C6" />
+                </td>
+                <td>
+                    <asp:Button ID="Button5" runat="server" Text="5" OnClick="Button5_Click" BackColor="#FFD9C6" />
+                </td>
+                <td>
+                    <asp:Button ID="Button6" runat="server" Text="6" OnClick="Button6_Click" BackColor="#FFD9C6" />
+                </td>
+                <td>
+                    <asp:Button ID="ButtonMultiplicar" runat="server" Text="*" OnClick="ButtonMultiplicar_Click" />
+                </td>
+            </tr>
+             <tr>
+                <td>
+                    <asp:Button ID="Button1" runat="server" Text="1" OnClick="Button1_Click" BackColor="#FFD9C6" />
+                </td>
+                <td>
+                    <asp:Button ID="Button2" runat="server" Text="2" OnClick="Button2_Click" BackColor="#FFD9C6" />
+                </td>
+                <td>
+                    <asp:Button ID="Button3" runat="server" Text="3" OnClick="Button3_Click" BackColor="#FFD9C6" />
+                </td>
+                <td>
+                    <asp:Button ID="ButtonRestar" runat="server" Text="-" OnClick="ButtonRestar_Click" />
+                </td>
+            </tr>
+              <tr>
+                <td>
+                    <asp:Button ID="Button0" runat="server" Text="0" OnClick="Button0_Click" BackColor="#FFD9C6" />
+                </td>
+                <td>
+                    <asp:Button ID="ButtonPunto" runat="server" Text="." OnClick="ButtonPunto_Click" BackColor="#FFD9C6" />
+                </td>
+                <td>
+                    <asp:Button ID="ButtonIgual" runat="server" Text="=" OnClick="ButtonIgual_Click" BackColor="#3399FF" />
+                </td>
+                <td>
+                    <asp:Button ID="ButtonSumar" runat="server" Text="+" OnClick="ButtonSumar_Click" />
+                </td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplicationCalculadora/Default.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationCalculadora
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonAbrirParentesis_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void ButtonCerrarParentesis_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void ButtonPorcentaje_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void ButtonBorrar_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text = "";
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void Button8_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void ButtonDividir_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void ButtonMultiplicar_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void ButtonRestar_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void Button0_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void ButtonPunto_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void ButtonSumar_Click(object sender, EventArgs e)
+        {
+            TextBoxExpresionAritmetica.Text += ((Button)sender).Text;
+        }
+
+        protected void ButtonIgual_Click(object sender, EventArgs e)
+        {
+            const string ERROR_MSG = "Error";
+            try
+            {
+                string expre = TextBoxExpresionAritmetica.Text;
+                //double result = Convert.ToDouble(new DataTable().Compute("(3+3)*2+1", null));
+                //El método “Compute” es el responsable de evaluar la expresión aritmética almacenada en un string.
+                double result = Convert.ToDouble(new System.Data.DataTable().Compute(expre, null));                
+                TextBoxExpresionAritmetica.Text = result.ToString().Replace(",", ".");
+            }
+            catch 
+            {
+                TextBoxExpresionAritmetica.Text = ERROR_MSG;
+            }               
+        }
+    }
+}
